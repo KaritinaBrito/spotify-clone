9762a65e95e7b0f35ebbd103448b483ccdda7444
KaritinaBrito <110512795+KaritinaBrito@users.noreply.github.com>
2024-09-12 16:28:30 -0600

    Update App.jsx

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -9,7 +9,7 @@ import ListAlbum from './pages/ListAlbum';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 
-export const url = 'http://localhost:4000';
+export const url = 'https://spotify-clone-backend-8s2u.onrender.com';
 
 const App = () => {
   return (
